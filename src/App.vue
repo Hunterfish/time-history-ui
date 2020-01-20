72473363e47b3e8091d2f88c356aa435034e82f9
lizh <2483433169@qq.com>
2020-01-20 13:17:51 +0800

    s;lflflslfl

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <router-view name="header"></router-view>
-        <main>
+        <main class="mainCon">
             <fade-transition origin="center" mode="out-in" :duration="250">
                 <router-view/>
             </fade-transition>
@@ -18,3 +18,12 @@ export default {
   }
 };
 </script>
+<style>
+    html,body{
+        width: 100%;
+        height: 100%;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(150deg, #cbaabc 15%, #856ed5 70%, #d98e82 94%);
+    }
+</style>
